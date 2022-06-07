1b42e0636fb5092940d5cee8d51b9d80ec7379b1
agustina <agussrojas.00@gmail.com>
2022-06-06 22:46:36 -0300

    segundo commit

--- javascript.js
+++ javascript.js
@@ -4,15 +4,15 @@ function calcule (precio, cant, provincia) {
     switch(provincia){
         case "salta":
             document.getElementById("provincia").innerHTML = 'Salta';
-            document.getElementById("cuotas").innerHTML = cant + " cuotas de: " + total;
+            document.getElementById("cuotas").innerHTML = cant + " cuotas de: " + total.toLocaleString('es-AR');
             break;
         case "scdbariloche":
             document.getElementById("provincia").innerHTML = 'San carlos de bariloche';
-            document.getElementById("cuotas").innerHTML = cant + " cuotas de: " + total;
+            document.getElementById("cuotas").innerHTML = cant + " cuotas de: " + total.toLocaleString('es-AR');
             break;
         case "mendoza":
             document.getElementById("provincia").innerHTML = 'Mendoza';
-            document.getElementById("cuotas").innerHTML = cant + " cuotas de: " + total;
+            document.getElementById("cuotas").innerHTML = cant + " cuotas de: " + total.toLocaleString('es-AR');
             break;
         default:
             break;
