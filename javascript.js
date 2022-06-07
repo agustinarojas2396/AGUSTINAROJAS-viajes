86e7736af1c7eaf07dce828ceb9b4a3c72bcfc7c
agustina <agussrojas.00@gmail.com>
2022-06-06 21:50:56 -0300

    primer commit

---
+++ javascript.js
@@ -0,0 +1,20 @@
+function calcule (precio, cant, provincia) {
+    let total = Math.trunc(precio / cant);
+    document.getElementById("section").style.display = 'inline-block';
+    switch(provincia){
+        case "salta":
+            document.getElementById("provincia").innerHTML = 'Salta';
+            document.getElementById("cuotas").innerHTML = cant + " cuotas de: " + total;
+            break;
+        case "scdbariloche":
+            document.getElementById("provincia").innerHTML = 'San carlos de bariloche';
+            document.getElementById("cuotas").innerHTML = cant + " cuotas de: " + total;
+            break;
+        case "mendoza":
+            document.getElementById("provincia").innerHTML = 'Mendoza';
+            document.getElementById("cuotas").innerHTML = cant + " cuotas de: " + total;
+            break;
+        default:
+            break;
+    }
+}
---
+++ media/javascript.js
@@ -0,0 +1,7 @@
+
+
+function calcule (total, cant) {    
+
+     document.getElementById("saltaPrice3").innerHTML = document.getElementById("saltaPrice3").innerHTML + total / cant;
+     
+}
